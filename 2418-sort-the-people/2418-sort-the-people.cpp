638c8009f798c333330ebea84c8b46495e9665fb
Sonalijatav <146960799+Sonalijatav@users.noreply.github.com>
2024-07-22 20:47:12 +0530

    Time: 608 ms (5.08%), Space: 92 MB (5.03%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        
+        int maxi = INT_MIN;
+        
+        for(int i=0;i<heights.size();i++){
+              for(int j=i+1;j<heights.size();j++){
+                if(heights[i]<heights[j])
+                {
+                  int temp = heights[i];
+                  heights[i] = heights[j];
+                  heights[j] = temp;
+                    
+                  string s= names[i];
+                  names[i] = names[j];
+                  names[j] = s;
+               }
+
+            }
+            cout<<heights[i]<<" ";
+    }
+        
+
+     return names;            
+    }
+};
